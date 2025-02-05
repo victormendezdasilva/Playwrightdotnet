1b1c016e0f65077785ccf009af379cf6d7114772
Victor Mendez <victormendezdasilva@gmail.com>
2025-02-05 17:55:57 -0300

    Initial Setup and Tests

---
+++ NUnitPlayrwight.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Playwright.NUnit;
+
+namespace Playwrightdotnet;
+
+public class NUnitPlaywright : PageTest
+{
+    [SetUp]
+    public async Task Setup()
+    {
+        await Page.GotoAsync("http://www.eaapp.somee.com");
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        await Page.ClickAsync("text=Login");
+
+        await Page.FillAsync("#UserName", "admin");
+        await Page.FillAsync("#Password", "password");
+        await Page.ClickAsync("text='Log in'");
+        await Expect(Page.Locator("text='Employee Details'")).ToBeVisibleAsync();
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Playwright;
+
+namespace Playwrightdotnet;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        using var playwright = await Playwright.CreateAsync();
+
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("http://www.eaapp.somee.com");
+        await page.ClickAsync("text=Login");
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "EAApp.jpg"
+        });
+
+        await page.FillAsync("#UserName", "admin");
+        await page.FillAsync("#Password", "password");
+        await page.ClickAsync("text='Log in'");
+        var isExist = await page.Locator("text='Employee Details'").IsVisibleAsync();
+        Assert.That(isExist);
+    }
+}
